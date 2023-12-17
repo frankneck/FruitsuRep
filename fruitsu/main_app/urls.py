85cc71541bc8b087b1bb7fd100e317850d36c03f
AnpaZz <zubkov.apa@students.dvfu.ru>
2023-12-18 03:02:32 +1000

    Пипец много изменений

--- fruitsu/main_app/urls.py
+++ fruitsu/main_app/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('UserProfile/', UserProfile),
-
+    path('EmptyPage/', EmptyPage),
+    path('Recipe/', Recipe),
+    path('ListOfRecipes/', ListOfRecipes),
 ]
--- fruitsu/main_app/views.py
+++ fruitsu/main_app/views.py
@@ -27,6 +27,14 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+def EmptyPage(request):
+    return render(request, 'main_app/EmptyPage.html')
+
+def Recipe(request):
+    return render(request, 'main_app/Recipe.html')
+
+def ListOfRecipes(request):
+    return render(request, 'main_app/ListOfRecipes.html')
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'О клубе Python Bytes'})
