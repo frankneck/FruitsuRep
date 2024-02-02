2ce8bc40ee3380a505b6d827ed80ac35b147f9f2
VladIsLoveGo <128213362+VladIsLoveGo@users.noreply.github.com>
2024-02-03 02:31:23 +1000

    Можно доабвлять картинку профилю, но пока только черенз админку

--- fruitsu/users/models.py
+++ fruitsu/users/models.py
@@ -2,9 +2,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth import get_user_model
 from django import forms
+from django.template.defaultfilters import slugify
+import os
 
 
 class CustomUser(AbstractUser):
+    def image_upload_to(self, instance=None):
+        if instance:
+            return os.path.join("Users", self.username, instance)
+        return None
 
     STATUS = (
         ('regular', 'regular'),
@@ -15,6 +21,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     status = models.CharField(max_length=100, choices=STATUS, default='regular')
     description = models.TextField('Description', max_length=600, default='', blank=True)
+    image = models.ImageField(default='default/no_image.png', upload_to=image_upload_to)
 
     def __str__(self):
         return self.username
